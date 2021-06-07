7430045a5b1bd6ea78cc5fdf3758777d3aca47e8
Your Name <you@example.com>
2021-06-07 12:05:39 +0600

    added anything write you

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWV7tIDNibf0VLUphuAlaCu59JHaFxKx4",
+  authDomain: "transport-service-bfe74.firebaseapp.com",
+  projectId: "transport-service-bfe74",
+  storageBucket: "transport-service-bfe74.appspot.com",
+  messagingSenderId: "123896940293",
+  appId: "1:123896940293:web:b1642eda29b5389bc2196c"
+};
+export default firebaseConfig;
